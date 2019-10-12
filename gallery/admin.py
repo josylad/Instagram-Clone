96028cd23eeb3193d1c739aeb23e140f9237972b
Joseph Adediji <josylado@gmail.com>
2019-10-12 21:14:15 +0300

    register the user profile model and functionality in the admin.py for admin access

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Location, Category, Image, Comment
+from .models import Location, Category, Image, Comment, Profile
 
 
 # Register your models here.
@@ -8,3 +8,4 @@ admin.site.register(Location)
 admin.site.register(Category)
 admin.site.register(Image)
 admin.site.register(Comment)
+admin.site.register(Profile)
