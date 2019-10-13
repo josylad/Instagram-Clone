796bfd2630250724b0641fd3670ab47945672d27
Joseph Adediji <josylado@gmail.com>
2019-10-13 10:17:50 +0300

    Update Admin.py file

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
-from .models import Location, Category, Image, Comment, Profile
+from .models import Image, Comment, Profile
 
 
 # Register your models here.
 
-admin.site.register(Location)
-admin.site.register(Category)
 admin.site.register(Image)
 admin.site.register(Comment)
 admin.site.register(Profile)
